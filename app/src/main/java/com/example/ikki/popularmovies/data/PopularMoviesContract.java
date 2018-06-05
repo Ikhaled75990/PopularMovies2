edaf8c94426dcdc6da39091f6aadb0771eba9774
Ikhaled75990 <ikramulkhaled@gmail.com>
2018-06-05 12:02:44 +0100

    latest commit for Entry

--- app/src/main/java/com/example/ikki/popularmovies/data/PopularMoviesContract.java
+++ app/src/main/java/com/example/ikki/popularmovies/data/PopularMoviesContract.java
@@ -1,5 +1,6 @@
 package com.example.ikki.popularmovies.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -14,6 +15,19 @@ public class PopularMoviesContract {
     public static final class PopularMoviesEntry implements BaseColumns{
 
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MOVIES);
+
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+
+        public static final String TABLE_NAME = "popularmovies";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_MOVIE_ID = "MovieID";
+        public static final String COLUMN_MOVIE_TITLE = "Title";
+        public static final String COLUMN_MOVIE_IMAGE = "Image";
+        public static final String COLUMN_MOVIE_RATING = "Rating";
+        public static final String COLUMN_MOVIE_RELEASE_DATE = "ReleaseDate";
+        public static final String COLUMN_MOVIE_SYNOPSIS = "Synopsis";
 
 
 
