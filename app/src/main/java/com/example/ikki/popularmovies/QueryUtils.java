7c6d6dc47af605b2738f8aa5b3a322bbf9aa74a4
Ikhaled75990 <ikramulkhaled@gmail.com>
2018-06-26 10:29:05 +0100

    PopularMovies2- Final

--- app/src/main/java/com/example/ikki/popularmovies/QueryUtils.java
+++ app/src/main/java/com/example/ikki/popularmovies/QueryUtils.java
@@ -21,7 +21,7 @@ public class QueryUtils {
 
     private static final String POPULAR_MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie";
     private static final String API = "api_key";
-    private static final String API_KEY = "96c1dbfb8dd93fd10fb7f4a9892a08e7";
+    private static final String API_KEY = "";
     private static final String ORIGINAL_TITLE = "original_title";
     private static final String POSTER_URL = "http://image.tmdb.org/t/p/w342/";
     private static final String MOVIE_POSTER = "poster_path";
